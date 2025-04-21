5856b02a877b42b10d82dd62689e016c834359c7
mohamadalabsi <absimoda1@gmail.com>
2025-04-21 21:09:01 +0200

    Starting Docker

---
+++ src/main/java/org/me/learning/docker/DockerApplication.java
@@ -0,0 +1,13 @@
+package org.me.learning.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerApplication.class, args);
+	}
+
+}
