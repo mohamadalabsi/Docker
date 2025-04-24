80c8279d1ebcaa14f2a765a0f3e2f789c27f421a
mohamadalabsi <absimoda1@gmail.com>
2025-04-24 22:52:58 +0200

    some notes

--- src/main/java/org/me/learning/docker/DockerApplication.java
+++ src/main/java/org/me/learning/docker/DockerApplication.java
@@ -9,5 +9,6 @@ public class DockerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DockerApplication.class, args);
 	}
-
+// i am about to finish docker , after that i will learn graphQl + redis , with that i will finish sprig boot completely
+//	after that , i will start with full backend project including everything i learned and make it for the whole semester
 }
